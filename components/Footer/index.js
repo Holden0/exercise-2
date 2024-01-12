bdff94abb41f400adbc7b4b652bc1467ae5c0fbe
Holden Budiman <holdenbudiman@Holdens-MacBook-Pro.local>
2024-01-11 20:22:13 -0800

    Adding files and folders

---
+++ components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+import link from 'next/link'
+
+export default function Footer() {
+    return(
+        <footer className={styles.footer}>
+            Created By Holden
+        </footer>
+
+    )
+}
